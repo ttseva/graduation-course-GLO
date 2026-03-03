13bac9fdda890c48f66a684d48f1da9445261f1a
Anastasia S. <annihilcax@gmail.com>
2026-03-04 01:44:21 +0300

    feat: modal windows

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+import modal from './modules/modal.js'
 
+const modalHeader = {
+  modalBlock: '.header-modal',
+  attr: '#callback'
+};
+const modalCallback = {
+  modalBlock: '.services-modal',
+  attr: '#application'
+};
 
+const modalOrder = {
+  modalBlock: '.discount-modal',
+  attr: '#order'
+};
 
+modal(modalHeader);
+modal(modalCallback);
+modal(modalOrder);
---
+++ src/modules/modal.js
@@ -0,0 +1,33 @@
+const modal = ({ modalBlock, attr }) => {
+  const modal = document.querySelector(modalBlock);
+  const overlay = document.querySelector('.overlay');
+  console.log(modal)
+
+  if (!modal) return;
+
+  const openModal = (modal, attr) => {
+    const modalBtn = document.querySelectorAll(`a[href="${attr}"]`);
+    modalBtn.forEach(btn => {
+      btn.addEventListener('click', event => {
+        event.preventDefault();
+        modal.style.display = 'block';
+        overlay.style.display = 'block';
+      })
+    })
+  }
+
+  const closeModal = modal => {
+    document.addEventListener('click', (e) => {
+      const closeBtn = modal.querySelector('[title="Close"]');
+      if (e.target === overlay || e.target === closeBtn) {
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+      }
+    })
+  }
+
+  openModal(modal, attr);
+  closeModal(modal);
+}
+
+export default modal
