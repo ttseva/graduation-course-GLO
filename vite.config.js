4d066e6586a05dd8356e9f3128409fcd631aad10
Anastasia S. <annihilcax@gmail.com>
2026-03-10 22:21:04 +0300

    final build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/graduation-course-GLO/',
   build: {
     rollupOptions: {
       input: {
